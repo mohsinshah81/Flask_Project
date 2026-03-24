6553fb8577fcabed607c258b61f9e67b6b39081f
mohsinshah81 <mohs.shah81@gmail.com>
2026-03-23 20:04:08 -0400

    Initial Commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    """
+    Sends text to the Watson NLP EmotionPredict API
+    and returns the 'text' attribute from the response.
+    """
+    
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+      # Build the input JSON
+    Json = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    # Send POST request
+    response = requests.post(url, headers=Headers, json=Json)
+
+   # Parsing the JSON response from the API 
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    #print(emotions)
+    #print(dominant_emotion)
+
+    return {
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"],
+        "dominant_emotion": dominant_emotion
+    }
